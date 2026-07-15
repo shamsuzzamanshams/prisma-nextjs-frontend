f0832e5f9ee2cce1d4fbb6d56c7acb6cb9f991e2
shamsuzzamanshams <shams30200@gmail.com>
2026-07-16 00:13:45 +0600

    link and navigation

---
+++ app/blog/[slug]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+	return (
+		<div>
+			blogs Loader........
+		</div>
+	)
+}
+
+export default loading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
     <div>
       Hello Next Js
+
+      Blog page <Link href={"/blog/1"}>Blog</Link>
     </div>
   );
 }
